3dbffaa962fed40faf9a667beeb0501086694ca5
msinha569 <msinha569@gmail.com>
2025-01-18 02:21:04 +0530

    landing page initiated

--- app/page.js
+++ app/page.js
@@ -1,13 +1,18 @@
+'use client'
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter()
+  useEffect(() => {
+    router.push('/dashboard')
+  },[])
   return (
   <div>
     
-    <Button >hey</Button>
-    <UserButton/>
   </div>
   );
 }
